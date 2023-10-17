3a3a34e566c7f8e13c2a968eeefd5ba3daf0dd2d
TheValerich <aleksey_safonov_90@mail.ru>
2023-10-17 12:17:32 +0300

    Added model of Category

--- agency/models.py
+++ agency/models.py
@@ -2,21 +2,8 @@ from django.db import models
 
 
 class Estate(models.Model):
-    class NamesChoices(models.TextChoices):
-        HOUSE = 'house', 'Дом'
-        FLAT = 'flat', 'Квартира'
-        SECTOR = 'sector', 'Участок'
-        COMM = 'commercial', 'Коммерческий'
-        RENT = 'rent', 'Аренда'
-        GARAGE = 'garage', 'Гараж'
-        ROOM = 'room', 'Комната'
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='estates')
 
-    name = models.CharField(
-        max_length=10,
-        default=NamesChoices.HOUSE,
-        choices=NamesChoices.choices,
-        verbose_name='Наименование'
-    )
     area = models.FloatField(verbose_name='Общая площадь')
     rooms = models.IntegerField(verbose_name='Количество комнат', default=1)
     material = models.CharField(max_length=128, verbose_name='Материал стен')
@@ -26,9 +13,21 @@ class Estate(models.Model):
     available = models.BooleanField(default=True, verbose_name='Доступен к продаже?')
 
     class Meta:
-        ordering = ['area']
+        ordering = ['available']
         verbose_name = 'Объект'
         verbose_name_plural = 'Объекты'
 
     def __str__(self):
-        return f'{self.name.capitalize()} - {self.rooms} ком., {self.area} м2'
+        return f'{Category.name.capitalize()} - {self.rooms} ком., {self.area} м2'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
