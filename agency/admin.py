d8a088b636fc5a177d818f338d995c02cb1c25ee
TheValerich <aleksey_safonov_90@mail.ru>
2023-10-17 12:17:00 +0300

    Added model of Category on admin

--- agency/admin.py
+++ agency/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Estate
+from .models import Estate, Category
 
 
 @admin.register(Estate)
 class EstateAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rooms', 'area', 'available']
-    list_filter = ['name', 'rooms', 'area', 'material', 'available']
+    list_display = ['rooms', 'area', 'available']
+    list_display_links = None
+    list_filter = ['rooms', 'area', 'material', 'available']
     list_editable = ['rooms', 'available']
+    # prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
