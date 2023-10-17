7270f8414148c96fac3b9b42bb5b35cb9a431138
TheValerich <aleksey_safonov_90@mail.ru>
2023-10-17 11:12:03 +0300

    Added model of Estate on admin

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Estate
 
-# Register your models here.
+
+@admin.register(Estate)
+class EstateAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rooms', 'area', 'available']
+    list_filter = ['name', 'rooms', 'area', 'material', 'available']
+    list_editable = ['rooms', 'available']
+    prepopulated_fields = {'slug': ('name',)}
