943eea04fe7a3d5741fb18ec9ec9e9b63e8b98df
TheValerich <aleksey_safonov_90@mail.ru>
2023-10-24 14:44:04 +0300

    Added name,best_offer,get_absolute

--- agency/models.py
+++ agency/models.py
@@ -1,29 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Estate(models.Model):
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='estates')
-
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='estates', verbose_name='Категория')
+    name = models.CharField(max_length=64, verbose_name='Название', default='')
     area = models.FloatField(verbose_name='Общая площадь')
     rooms = models.IntegerField(verbose_name='Количество комнат', default=1)
-    material = models.CharField(max_length=128, verbose_name='Материал стен')
+    material = models.CharField(max_length=128, verbose_name='Материал стен', blank=True)
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(max_length=200, verbose_name='Слаг')
     image = models.ImageField(upload_to='agency/%Y/%m/%d', blank=True, verbose_name='Изображение')
     available = models.BooleanField(default=True, verbose_name='Доступен к продаже?')
+    best_offer = models.BooleanField(default=False, verbose_name='Лучшее предложение')
 
     class Meta:
-        ordering = ['available']
+        ordering = ['-available']
         verbose_name = 'Объект'
         verbose_name_plural = 'Объекты'
 
     def __str__(self):
-        return f'{Category.name.capitalize()} - {self.rooms} ком., {self.area} м2'
+        return f'{self.name.capitalize()} - {self.rooms} ком., {self.area} м2'
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=64)
-    slug = models.SlugField(max_length=64, unique=True)
+    name = models.CharField(max_length=64, verbose_name='Наименование')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
 
     class Meta:
         verbose_name = 'Категория'
@@ -31,3 +33,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            'agency:catalog_by_category_url',
+            args=[self.slug]
+        )
