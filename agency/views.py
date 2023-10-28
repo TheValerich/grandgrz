79b7206abb395db5a72184b24bf0fa1fc77c44eb
TheValerich <aleksey_safonov_90@mail.ru>
2023-10-28 14:00:18 +0300

    Added view for estate

--- agency/views.py
+++ agency/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Estate, Category
 
 
@@ -15,47 +16,48 @@ def requisites_view(request):
     return render(request, 'requisites.html')
 
 
-# def estate_list_view(request, category_slug=None):
-#     category = None
-#     categories = Category.objects.all()
-#     estates = Estate.objects.filter(available=True)
-#     if category_slug:
-#         category = get_object_or_404(
-#             Category,
-#             slug=category_slug
-#         )
-#         estates = estates.filter(category=category)
-#     return render(
-#         request,
-#         'catalog1.html',
-#         {
-#             'category': category,
-#             'categories': categories,
-#             'estates': estates,
-#         }
-#     )
-
-
-def estate_list_by_category_view(request):  # TODO добавить блок try-except
-    houses=Category.objects.get(name='Дома').estates.all().filter(available=True)
-    flates=Category.objects.get(name='Квартиры').estates.all().filter(available=True)
-    sectors=Category.objects.get(name='Участки').estates.all().filter(available=True)
-    garages=Category.objects.get(name='Гаражи').estates.all().filter(available=True)
-    commercials=Category.objects.get(name='Коммерческие').estates.all().filter(available=True)
-    rent=Category.objects.get(name='Аренда').estates.all().filter(available=True)
-    rooms=Category.objects.get(name='Комнаты').estates.all().filter(available=True)
-    # others=Category.objects.get(name='Прочие').estates.all().filter(available=True)
+def estate_list_by_category_view(request):
+    estates = Estate.objects.filter(available=True).select_related('category')
+    context = {
+        'houses': [],
+        'flates': [],
+        'sectors': [],
+        'garages': [],
+        'commercial': [],
+        'rent': [],
+        'rooms': [],
+        'others': [],
+    }
+    for estate in estates:
+        if estate.category.name == 'Дома':
+            context['houses'].append(estate)
+        elif estate.category.name == 'Квартиры':
+            context['flates'].append(estate)
+        elif estate.category.name == 'Участки':
+            context['sectors'].append(estate)
+        elif estate.category.name == 'Гаражи':
+            context['garages'].append(estate)
+        elif estate.category.name == 'Коммерческие':
+            context['commercial'].append(estate)
+        elif estate.category.name == 'Аренда':
+            context['rent'].append(estate)
+        elif estate.category.name == 'Комнаты':
+            context['rooms'].append(estate)
+        elif estate.category.name == 'Прочие':
+            context['others'].append(estate)
     return render(
         request,
         'catalog.html',
-        {
-            'houses': houses,
-            'flates': flates,
-            'sectors': sectors,
-            'garages': garages,
-            'commercial': commercials,
-            'rent': rent,
-            'rooms': rooms,
-            # 'others': others
-        }
+        context
     )
+
+
+class EstateDetailView(DetailView):
+    model = Estate
+    template_name = 'estate_detail.html'
+    context_object_name = 'estate'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['images'] = Estate.objects.get(id=self.object.pk).images.all()
+        return context
