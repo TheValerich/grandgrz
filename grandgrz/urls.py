fe12456fc6e6cb5b5f79bab22d901201fba55bb8
TheValerich <aleksey_safonov_90@mail.ru>
2023-11-08 14:02:28 +0300

    Added if not DEBUG

--- grandgrz/urls.py
+++ grandgrz/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.views import serve
+from django.views.static import serve as media_serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>', serve, {'insecure': True}))
+    urlpatterns.append(path('media/<path:path>', media_serve, {'document_root': settings.MEDIA_ROOT}))
